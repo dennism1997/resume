ca64838c1a70547788a26867f28564725341bdfc
dennism1997 <D.Mouwen@student.tudelft.nl>
2016-11-05 17:34:33 +0100

    skills part kinda done

---
+++ js/script.js
@@ -0,0 +1,39 @@
+/**
+ * Created by dennis on 05-11-16.
+ */
+$(function addScores() {
+    $(".score-1").append(
+        "<img width=\"24\" src=\"resources/images/star-blue.svg\"\>" +
+        "<img width=\"24\" src=\"resources/images/star-blue-transparant.svg\"\>" +
+        "<img width=\"24\" src=\"resources/images/star-blue-transparant.svg\"\>" +
+        "<img width=\"24\" src=\"resources/images/star-blue-transparant.svg\"\>" +
+        "<img width=\"24\" src=\"resources/images/star-blue-transparant.svg\"\>");
+
+    $(".score-2").append(
+        "<img width=\"24\" src=\"resources/images/star-blue.svg\"\>" +
+        "<img width=\"24\" src=\"resources/images/star-blue.svg\"\>" +
+        "<img width=\"24\" src=\"resources/images/star-blue-transparant.svg\"\>" +
+        "<img width=\"24\" src=\"resources/images/star-blue-transparant.svg\"\>" +
+        "<img width=\"24\" src=\"resources/images/star-blue-transparant.svg\"\>");
+
+    $(".score-3").append(
+        "<img width=\"24\" src=\"resources/images/star-blue.svg\"\>" +
+        "<img width=\"24\" src=\"resources/images/star-blue.svg\"\>" +
+        "<img width=\"24\" src=\"resources/images/star-blue.svg\"\>" +
+        "<img width=\"24\" src=\"resources/images/star-blue-transparant.svg\"\>" +
+        "<img width=\"24\" src=\"resources/images/star-blue-transparant.svg\"\>");
+
+    $(".score-4").append(
+        "<img width=\"24\" src=\"resources/images/star-blue.svg\"\>" +
+        "<img width=\"24\" src=\"resources/images/star-blue.svg\"\>" +
+        "<img width=\"24\" src=\"resources/images/star-blue.svg\"\>" +
+        "<img width=\"24\" src=\"resources/images/star-blue.svg\"\>" +
+        "<img width=\"24\" src=\"resources/images/star-blue-transparant.svg\"\>");
+
+    $(".score-5").append(
+        "<img width=\"24\" src=\"resources/images/star-blue.svg\"\>" +
+        "<img width=\"24\" src=\"resources/images/star-blue.svg\"\>" +
+        "<img width=\"24\" src=\"resources/images/star-blue.svg\"\>" +
+        "<img width=\"24\" src=\"resources/images/star-blue.svg\"\>" +
+        "<img width=\"24\" src=\"resources/images/star-blue.svg\"\>");
+});
